c9ced709eb4fb6f4998e4e24717c1f2ae4be22e7
GarkushaMV <m.v.garkusha@gmail.com>
2015-03-26 14:06:32 +0300

    Update bignum_big_sum.c

--- II семестр/Системы счисления/bignum_big_sum.c
+++ II семестр/Системы счисления/bignum_big_sum.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
 
-char c;
-int a[1001], b[1001];
-int t;
 
+int main()
+{
+    char c = '0';
+    int a[1001], b[1001];
+    int t = 0;
+    int k = 0;
+    int i = 1000;
+    while (i >= 0)
+    {
+        a[i] = 0;
+        b[i] = 0;
+        i--;
+    }
+    i = 1000;
+    while (c == '0')
+        scanf("%c", &c);
+    while ((c == '0') || (c == '1'))
+    {
+        a[i] = c - '0';
+        scanf("%c", &c);
+        i--;
+    }
+    i++;
+    for (k = 1000; 2 * k  > 1000 + i; k--)
+    {
+        t = a[k];
+        a[k] = a[1000 + i - k];
+        a[1000 + i - k] = t;
+    }
 
-int main(void) {
-	int k;
+    i = 1000;
+    scanf("%c", &c);
+    while ((c == '0') || (c == '1'))
+    {
+        b[i] = c - '0';
+        scanf("%c", &c);
+        i--;
+    }
+    i++;
+    for (k = 1000; 2 * k  > 1000 + i; k--)
+    {
+        t = b[k];
+        b[k] = b[1000 + i - k];
+        b[1000 + i - k] = t;
+    }
 
-	int i = 1000;
-	scanf("%c", &c);
-	while ((c == '0') || (c == '1')) {
-		a[i] = c - '0';
-		scanf("%c", &c);
-		i--;
-	}
-	i++;
-	for (k = 1000; 2 * k  > 1000 + i; k--) {
-		t = a[k];
-		a[k] = a[1000 + i - k];
-		a[1000 + i - k] = t;
-	}
+    for (i = 1000; i > 0; i--)
+    {
+        a[i]   += b[i];
+        a[i-1] += a[i] / 2;
+        a[i]   %= 2;
+    }
 
-	i = 1000;
-	scanf("%c", &c);
-        while ((c == '0') || (c == '1')) {
-                b[i] = c - '0';
-                scanf("%c", &c);
-		i--;
-        }
-	i++;
-        for (k = 1000; 2 * k  > 1000 + i; k--) {
-                t = b[k];
-                b[k] = b[1000 + i - k];
-                b[1000 + i - k] = t;
-        }
+    i = 0;
+    while ((a[i] == 0) && (i < 1000))
+        i++;
 
-	for (i = 1000; i > 0; i--) {
-		a[i] += b[i];
-		a[i-1] += a[i] / 2;
-		a[i] %= 2;
-	}
+    for (; i < 1001; i++)
+        printf("%d", a[i]);
 
-	i = 0;
-	while ((a[i] == 0) && (i < 1000))
-		i++;
-
-	for (; i < 1001; i++)
-		printf("%d", a[i]);
-
-	return 0;
+    return 0;
 }
